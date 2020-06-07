19634d83ddfb03c06035013f96a9336b0690da9f
Zach Kilburn <zachkilburn@Zach-Kilburns-MacBook-Pro.local>
2020-06-07 08:35:17 -0500

    basic working dashboard

--- rowingconditions/plotlydash/dashboard.py
+++ rowingconditions/plotlydash/dashboard.py
@@ -1,5 +1,11 @@
 import dash
+import dash_core_components as dcc
 import dash_html_components as html
+import plotly.express as px
+import plotly.graph_objects as go
+from rowingconditions.resources.arkansas_data import last_30_days
+
+df = last_30_days()
 
 def create_dashboard(server):
     
@@ -8,6 +14,37 @@ def create_dashboard(server):
                          external_stylesheets=['/static/css/styles.css']
                          )
 
-    dash_app.layout = html.Div(id='dash-container')
+    trace1 = go.Scatter(x=df['date_time'],
+                        y=df['gage_height'],
+                        name='Gage Height',
+                        mode='lines+markers',
+                        yaxis='y1'
+    )
+
+    trace2 = go.Scatter(x=df['date_time'],
+                        y=df['stream_flow'],
+                        name='Stream Flow',
+                        mode='lines+markers',
+                        yaxis='y2'
+    )
+
+    data = [trace1, trace2]
+
+    layout = go.Layout(title='Stream Flow and Gage Height Last 30 Days',
+                        yaxis=dict(title='Gage Height (ft)'),
+                        yaxis2=dict(title='Stream Flow (cfs)',
+                                    overlaying='y',
+                                    side='right')
+    )
+
+    dash_app.layout = html.Div(children=[
+        html.H1(children='Arkansas River'),
+
+        html.P(children='''
+            Dash: A web application framework for Python.
+        '''),
+
+        dcc.Graph(figure=go.Figure(data=data, layout=layout))
+    ])
 
     return dash_app.server
